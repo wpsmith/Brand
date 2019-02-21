1ccd6e5fe8d81e3b562b67f89a412fa99163b8f3
wpsmith <t@wpsmith.net>
2019-02-20 19:30:16 -0500

    Update namespace/docs

--- src/Brand.php
+++ src/Brand.php
@@ -8,23 +8,25 @@
  * Any modifications to or software including (via compiler) GPL-licensed code must also be made
  * available under the GPL along with build & install instructions.
  *
- * @package    WPS\Core
+ * @package    WPS\WP
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
  * @since      0.1.0
  */
 
-namespace WPS\Core;
+namespace WPS\WP;
+
+use WPS\Core\Singleton;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'WPS\Core\Brand' ) ) {
+if ( ! class_exists( __NAMESPACE__ . '\Brand' ) ) {
 	/**
 	 * Branding Class
 	 *
